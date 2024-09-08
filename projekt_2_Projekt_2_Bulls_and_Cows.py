34381333a2595d14d0e8d12b4f690ea21efb5dc9
Khadnin <47931907+Khadnin@users.noreply.github.com>
2024-09-08 16:20:28 +0200

    Reworked code to count cows properly and more readable

--- projekt_2_Projekt_2_Bulls_and_Cows.py
+++ projekt_2_Projekt_2_Bulls_and_Cows.py
@@ -10,6 +10,42 @@ discord: khadnin
 import random
 import time
 
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+# Všechny funkce
+
+# Funkce na otestování zda je vstup číslo, zda jsou všechny číslice unikátní, zda délka má požadovanou délku, a zároveň že není první číslice nula
+def guess_correct(num):
+    if num.isdigit() and (len(set(str(num))) == number_length) and (len(str(num)) == number_length) and (num[0] != "0") == True:
+        return True
+    else:
+        return False
+
+# Funkce na vytvoření seznamu číslic ze zadaného čísla
+def make_num_to_list(number):
+    return [digit for digit in str(number)]
+
+# Funkce na vrácení stráveného času hádáním
+def time_result():
+    end_time = round(abs(start_time - time.time()), 1)
+    return(print("It took you", end_time, "s.\n") )
+
+# Funkce pro zobrazení počtu pokusů (podle počtu pokusů)
+def attempt_result(attempts_amount):
+    if attempts_amount == 1:
+        return(print(
+            "Correct, you've guessed the right number in ", attempts, " quess!\n"
+            "-----------------------------------------------------------------"
+            )
+        )
+    elif attempts_amount > 1:
+        return(print(
+            "Correct, you've guessed the right number in ", attempts, " quesses!\n"
+            "-----------------------------------------------------------------"
+            )
+        )
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+
 # Délka čísla, které se bude hádat (max 10!)
 number_length = 4
 
@@ -22,7 +58,7 @@ first_digit = random.randint(1, 9)
 # Odstranění první číslice z možných číslic pro další výběr
 list_of_digits.remove(first_digit)
 
-# Převod prvního čísla na text, z důvodů budoucího spojování generovancýh číslic
+# Převod prvního čísla na text, z důvodů budoucího spojování generovaných číslic
 final_number = str(first_digit)
 
 # Pro zvolenou délku čísla náhodný výběr ze seznamu číslic, vymazaní vybrané číslice ze seznamu ať se neopakuje a připojení k existujícímu číslu
@@ -30,6 +66,8 @@ for i in range(number_length - 1):
     random_digit = random.choice(list_of_digits)
     final_number = final_number + (str(random_digit))
     list_of_digits.remove(random_digit)
+
+print(final_number)
 
 # "Uvítání soutěžícího" :)  
 print(
@@ -43,64 +81,51 @@ print(
 # Definování proměnných pro počítání počtu pokusů, času a podmínky ukončení while cyklu
 attempts = 0
 start_time = time.time()
-x = 1
+active_game = True
 
-while x == 1:
+while active_game == True:
     guess = input("Enter a number (in case you want to exit, wrote 'exit'): ")
     bulls = 0
     cows = 0
 # Podmínka pro možné ukončení než člověk přijde na výsledek
     if guess == "exit":
-        x = 0
+        active_game = False
         print("Terminating script. The number was:", final_number)
-# Otestování zda je vstup číslo, zda jsou všechny číslice unikátní, zda délka má požadovanou délku, a zároveň že není první číslice nula
-    elif guess.isdigit() and (len(set(str(guess))) == number_length) and (len(str(guess)) == number_length) and (guess[0] != "0") == True:
-# Zjištění počtu "cows" skrz překrytí setů vstupu a generovaného čísla
-        guess_set = set(guess)
-        number_set = set(final_number)
-        cows = len(guess_set.intersection(number_set))
-        attempts += 1
-# Zjištění počtu "bulls" procházením jednotlivých číslic/cifer
+# Otestování vstupu
+    elif guess_correct(guess):
+        attempts += 1  
+# Převedení zadaného vstupu a hádaného čísla na seznamy pro porovnání
+        guess_list = make_num_to_list(guess)
+        number_list = make_num_to_list(final_number)
+# Cyklus pro procházení tolikrát jak je dlouhé hádané číslo 
         for i in range(len(final_number)):
-            if guess[i] == final_number[i]:
+# Kontrola "bulls" - zda je hádaná číslice v čísle rovna číslici na stejné pozici hádaného čísla
+            if guess_list[i] == number_list[i]:
                 bulls += 1
+# Kontrola "cows" - zda je hádaná číslice v čísle v hádaném čísle (seznamu)
+            elif guess_list[i] in number_list:
+                cows += 1
 # Zjišťování zda je číslo uhádnuto celé a podle počtu pokusů "vytiskne" výsledek nebo vypíše počet bulls a cows a pokračuje se
-        if bulls == number_length and attempts == 1:
-            print(
-                "Correct, you've guessed the right number in ", attempts, " quess!\n"
-                "-----------------------------------------------------------------\n"
-                "That's amazing!"
-                )
-            end_time = round(abs(start_time - time.time()), 1)
-            print("It took you", end_time, "s.")   
-            x = 0
-        elif bulls == number_length and attempts > 1 and attempts <= 5:
-            print(
-                "Correct, you've guessed the right number in ", attempts, " quesses!\n"
-                "-----------------------------------------------------------------\n"
-                "That's amazing!"
-                )
-            end_time = round(abs(start_time - time.time()), 1)
-            print("It took you", end_time, "s.")   
-            x = 0
-        elif bulls == number_length and attempts > 5 and attempts <= 15:
-            print(
-                "Correct, you've guessed the right number in ", attempts, " quesses!\n"
-                "-----------------------------------------------------------------\n"
-                "That's average!"
-                )
-            end_time = round(abs(start_time - time.time()), 1)
-            print("It took you", end_time, "s.")       
-            x = 0
+        if bulls == number_length:
+            attempt_result(attempts)
+            print("That's amazing!")
+            time_result()  
+            active_game = False
+        elif bulls == number_length and attempts > 2 and attempts <= 5:
+            attempt_result(attempts)
+            print("That's amazing!")
+            time_result()  
+            active_game = False
+        elif bulls == number_length and attempts > 6 and attempts <= 15:
+            attempt_result(attempts)
+            print("That's average!")
+            time_result()  
+            active_game = False
         elif bulls == number_length and attempts > 15:
-            print(
-                "Correct, you've guessed the right number in ", attempts, " quesses!\n"
-                "-----------------------------------------------------------------\n"
-                "That's not so good!"
-                )
-            end_time = round(abs(start_time - time.time()), 1)
-            print("It took you", end_time, "s.")        
-            x = 0
+            attempt_result(attempts)
+            print("That's not so good!")
+            time_result()  
+            active_game = False
         else:
             print(
                 "Bulls:", bulls, "\n"
